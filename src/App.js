272f2469aa9a6c7ef614b2c4ef30bc9d9b0a1e9b
Utku <48655033+utqkaba@users.noreply.github.com>
2022-03-15 17:11:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Timer from "./components/Timer";
+import Settings from "./components/Settings";
+import {useState} from "react";
+import SettingsContext from "./components/SettingsContext";
+
+function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
+  return (
+    <main className='App'>
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
+}
+
+export default App;
